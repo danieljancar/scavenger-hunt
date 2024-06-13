b47294c8ca064779459ec8256b1d9cac2dd042a2
MRX1621 <ahofer007@gmail.com>
2024-06-13 10:26:15 +0200

    fix(splash): set constraints for splash screen image and updated the home screen icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,25 @@ const config: CapacitorConfig = {
     appId: 'com.scavenger.hunt',
     appName: 'scavenger-hunt',
     webDir: 'www',
+
+    plugins: {
+        SplashScreen: {
+            launchShowDuration: 3000,
+            launchAutoHide: true,
+            launchFadeOutDuration: 3000,
+            backgroundColor: '#ffffffff',
+            androidSplashResourceName: 'splash',
+            androidScaleType: 'CENTER_CROP',
+            showSpinner: false,
+            androidSpinnerStyle: 'large',
+            iosSpinnerStyle: 'small',
+            spinnerColor: '#999999',
+            splashFullScreen: true,
+            splashImmersive: true,
+            layoutName: 'launch_screen',
+            useDialog: true,
+        },
+    },
 }
 
 export default config
